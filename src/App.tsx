3e17be56605d49cbcfde76fd2dae7813e717412f
Murilo Rodrigues <murilo.rgalvao23@gmail.com>
2022-06-21 23:15:36 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const {data} = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+
+  console.log(data);
+
   return (
-    <h1 className="text-5xl font-bold">Hello World</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
 export default App
+ 
